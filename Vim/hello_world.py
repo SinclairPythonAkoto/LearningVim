be737f075917796810afe4981a78149abe5b7568
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-03-02 19:19:02 +0000

    Python default file added

---
+++ Vim/hello_world.py
@@ -0,0 +1,6 @@
+# This file is to learn and practice my Vim commands.  
+# I will also create and modfity files using the Vim text editor.
+# This Pyhton file will default to print 'Hello World' unless changed.
+
+
+print("Hello World")
